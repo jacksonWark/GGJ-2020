d64033eddbb31f3f2d6b0521037cddd51cdbe236
jacksonWark <jackson@allanwark.com>
2020-02-01 17:10:43 -0700

    Some changes to menus. Create interactable interface and store script

---
+++ RoboRepair/Assets/Scripts/Store.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Store : MonoBehaviour, IInteractable
+{
+    #region Fields
+    public enum StoreType : int { HEAD, ARMS, LEGS, COGS}
+
+    public StoreType type;
+
+    private string infoString;
+
+    private int price;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        price = Random.Range(5, 101);
+        infoString = type.ToString() + " " + price.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string Detect()
+    {
+        return infoString;
+    }
+
+    public bool Interact()
+    {
+        return true;
+    }
+
+}
