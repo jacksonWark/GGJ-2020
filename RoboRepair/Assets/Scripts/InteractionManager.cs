3e64bd4000801f712bf9710fa0fdfb5c1ccda7dc
jacksonWark <jackson@allanwark.com>
2020-02-07 14:44:28 -0700

    Add random movement to robots; UI changes; comments on scripts

--- RoboRepair/Assets/Scripts/InteractionManager.cs
+++ RoboRepair/Assets/Scripts/InteractionManager.cs
@@ -14,15 +14,15 @@ public class InteractionManager : MonoBehaviour
     public Text[] parts = new Text[4];
 
     //public enum partTypes : int { HEAD, ARMS, LEGS, COGS }
-    int[] inventory;
+    int[] inventory = new int[] { 0, 0, 0, 0 };
     public int invSize;
-    int itemCount;
+    int itemCount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        inventory = new int[] { 0, 0, 0, 0 };
-        itemCount = 0;
+        //inventory = new int[] { 0, 0, 0, 0 };
+        //itemCount = 0;
         //UIText.enabled = false;
         //badText.enabled = false;
         foreach (Text t in parts)
@@ -36,12 +36,19 @@ public class InteractionManager : MonoBehaviour
         Drop();
     }
 
+    /// <summary>
+    /// Happens when we enter a store/robot detection collider and while we stay in it.
+    /// Handles recieving UI info and interacting with both stores and robots. Potentially refactor to make easier to read.
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerStay(Collider other)
     {
         IInteractable subject = other.gameObject.GetComponent<IInteractable>();
         if (subject != null)
         {
+            // Get info from robot or store
             string infoString = subject.Detect();
+            // Prepare info to displayed on UI
             string[] info = infoString.Split(' ');
             if(info[0] == "0") { UIText.text = "Head"; }
             else if(info[0] == "1") { UIText.text = "Arm"; }
@@ -53,6 +60,7 @@ public class InteractionManager : MonoBehaviour
             if (other.tag == "store") { UIText.text += "buy"; }
             else { UIText.text += "repair"; }
 
+            // Prepare info to be used later
             int num = Convert.ToInt32(info[1]);
             int index = Convert.ToInt32(info[0]);
 
@@ -61,17 +69,21 @@ public class InteractionManager : MonoBehaviour
 
             if (Input.GetKeyDown(KeyCode.E))
             {
+                // Is this a store or a robot
                 if (other.gameObject.tag == "store")
                 {
                     //Debug.Log(num.ToString() + " " + creditTracker.getCreditBalance().ToString());
+                    // Do we have enough inventory space
                     if (itemCount > 9)
                     {
                         badText.text = "Not enough space to carry!";
                     }
                     else
                     {
+                        // Do we have enough money
                         if (creditTracker.GetCreditBalance() >= num)
                         {
+                            // Remove credits and add parts to inventory
                             subject.Interact();
                             creditTracker.SpendCredits(num);
                             inventory[index]++;
@@ -87,20 +99,23 @@ public class InteractionManager : MonoBehaviour
                 }
                 else
                 {
+                    // Do we have enough on the appropriate part
                     if (inventory[index] > 0)
                     {
+                        // Were there no errors on the robot side of things
                         if (subject.Interact())
                         {
+                            // Remove parts from inventory and add credits
                             creditTracker.AddCredits(num);
                             inventory[index]--;
                             itemCount--;
                             RefreshUI();
+                            // Blank UI text since robot will be changing states shortly
                             UIText.text = "";
                         }
                     }
                     else
                     {
-                        //activate badText
                         badText.text = "You need more parts!";
                         badText.enabled = true;
                     }
@@ -109,6 +124,9 @@ public class InteractionManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Happens when we leave the detection collider of a robot or store. Blanks UI elements
+    /// </summary>
     private void OnTriggerExit()
     {
         //disable UIText
@@ -118,7 +136,9 @@ public class InteractionManager : MonoBehaviour
         badText.text = "";
     }
 
-
+    /// <summary>
+    /// Refresh UI for counting parts in inventory
+    /// </summary>
     void RefreshUI()
     {
         int i = 0;
@@ -129,6 +149,9 @@ public class InteractionManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Drop a specific part and refresh the number being displayed on the counter
+    /// </summary>
     void Drop()
     {
         if (Input.GetKeyUp(KeyCode.Alpha1))
--- RoboRepair/Assets/Scripts/Robot.cs
+++ RoboRepair/Assets/Scripts/Robot.cs
@@ -12,9 +12,13 @@ public class Robot : MonoBehaviour, IInteractable
     private float breakTime;
     private int reward;
 
+    // For random movement
+    private bool isIdle = true;
+    private float timer = 0;
+
     //Type of break
     // { 0 = HEAD, 1 = ARMS, 2 = LEGS, 3 = COGS, 4 = NOT BROKEN}
-    private int breakType;
+    private int breakType = 4;
 
     private void Awake()
     {
@@ -24,28 +28,40 @@ public class Robot : MonoBehaviour, IInteractable
         trigger.enabled = false;
     }
 
-    // Start is called before the first frame update
+    // Start is called before the first frame update. Generate a random interval, and start a timer that tells robot when to break
     void Start()
     {
-        breakType = 4;
         //breakTime = Random.Range(10f, 60f);
         breakTime = 5;
         StartCoroutine(BreakTimer());
     }
 
-    // Update is called once per frame
+    // Update is called once per frame. If we are broken, move randomly, otherwise patrol along waypoints
     void Update()
     {
         if (breakType == 4) waypointController.Move();
+        else RandomMovement();
+    }
+
+    private void FixedUpdate()
+    {
+        if (breakType != 4) // and player not nearby
+        {
+            timer -= Time.deltaTime;
+        }
     }
 
     private void Break()
     {
+        // Change animation to idle
         animator.SetBool("isIdle", true);
+        // Pick random type of break
         breakType = Random.Range(0, 4);
-        reward = Random.Range(5, 301);
+        // Set random reward for repair - TODO make this parameterized
+        reward = Random.Range(50, 301);
+        // Create string to broadcast information to player UI
         infoString = breakType.ToString() + " " + reward.ToString();
-        //enable trigger collider
+        //enable trigger collider so player can detect robot
         trigger.enabled = true;
     }
 
@@ -58,22 +74,52 @@ public class Robot : MonoBehaviour, IInteractable
     {
         if (breakType != 4)
         {
+            // Set to not broken
             breakType = 4;
+            // Create new random interval until next break
             breakTime = Random.Range(10f, 601f);
+            // Disable detection collider
             trigger.enabled = false;
+            // Start break interval timer
             StartCoroutine(BreakTimer());
+            // Set animation back to walking
             animator.SetBool("isIdle", false);
             return true;
         }
         else return false;
     }
 
+    private void RandomMovement()
+    {
+        if (!isIdle)
+        {
+            if (timer <= 0)
+            {
+                // Set state to idle, change animation, and wait a random amount of time
+                isIdle = true;
+                animator.SetBool("isIdle", true);
+                timer = Random.Range(1, 6);
+            }
+        }
+        else
+        {
+            if (timer <= 0)
+            {
+                // Set state to moving, turn in a random direction, change animation, and move for a random amount of time
+                transform.rotation = Quaternion.Euler(0, Random.Range(0.0f, 360.0f), 0);
+                timer = Random.Range(1, 6);
+                animator.SetBool("isIdle", false);
+                isIdle = false;
+            }
+        }
+    }
+
     private IEnumerator BreakTimer()
     {
-        Debug.Log("Start Timer");
+        //Debug.Log("Start Timer");
         //Wait for set amount of time before breaking
         yield return new WaitForSeconds(breakTime);
-        Debug.Log("End Timer");
+        //Debug.Log("End Timer");
         Break();
     }
 }
