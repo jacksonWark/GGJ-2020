30516d4022232f49a5099f6c5a1cb38d58722f73
jacksonWark <jackson@allanwark.com>
2020-02-26 16:03:50 -0700

    Make sound 3D; add footsteps (they suck)

---
+++ RoboRepair/Assets/Scripts/FootstepManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepManager : MonoBehaviour
+{
+
+    public AudioSource footstep;
+
+    public void OnFootstepEvent()
+    {
+        footstep.Play();
+    }
+}
